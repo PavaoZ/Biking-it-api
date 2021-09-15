eba9a5b5479afe63a934381e6613b799f96e5e9f
Pavo <pzornija1@etf.unsa.ba>
2021-09-15 20:29:56 +0200

    Initial project set up

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response, NextFunction } from 'express'
+// import "reflect-metadata";
+// import db from './database/models'
+// import * as routes from './routes/_index'
+
+const app = express()
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// routes.initRoutes(app)
+
+// db.sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+// })
+
+export default app
